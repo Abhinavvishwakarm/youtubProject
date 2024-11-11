fb27070f27681c6fc82b67791a7b7972f262cf15
abhinav vish <abhinav@koyaltechsolutions.com>
2024-11-11 10:45:25 +0530

    file upload setup using  cloudiany or fs node packages

---
+++ src/utils/cloudianry.js
@@ -0,0 +1,21 @@
+import {v2 as cloudianry} from 'cloudinary'
+import fs from 'fs'
+
+ cloudianry.config({ 
+        cloud_name: process.env.CLOUDIANY_CLOUD_NAME, 
+        api_key: process.env.CLOUDIANY_API_KEY, 
+        api_secret: process.env.CLOUDIANY_SECRET_KEY // Click 'View API Keys' above to copy your API secret
+    });
+
+    const uploadOnCloudnairy  = async (localFilePath) =>{
+        try {
+            if(!localFilePath) return null;
+            // upload file on cloudianary//
+            const response = await cloudianry.uploader.upload(localFilePath, {
+                resource_type: "auto",
+            })
+            console.info('file is uploaded on cloudaniary ', response.url)
+        } catch (error) {
+            
+        }
+    }
